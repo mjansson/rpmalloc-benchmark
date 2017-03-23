6abe4b5a3c51b83a832aa59e969d1d9d6469c206
Mattias Jansson <mattias@rampantpixels.com>
2017-03-23 23:33:52 +0100

    restore full suite

--- runall.sh
+++ runall.sh
@@ -2,19 +2,19 @@
 
 os="linux"
 
-for name in rpmalloc lockfree-malloc jemalloc tcmalloc scalloc crt nedmalloc; do
+for name in rpmalloc-unlimit rpmalloc jemalloc lockfree-malloc tcmalloc scalloc crt nedmalloc; do
 	executable=bin/$os/release/x86-64/benchmark-$name
 
 	$executable 1 0 0 2 50000 50000 1500 16 1000
 	$executable 2 0 0 2 50000 50000 1500 16 1000
 	$executable 3 0 0 2 50000 50000 1500 16 1000
-	#$executable 4 0 0 2 100000 50000 1500 16 1000
-	#$executable 5 0 0 2 100000 50000 1500 16 1000
-	#$executable 6 0 0 2 100000 50000 1500 16 1000
-	#$executable 7 0 0 2 100000 50000 1500 16 1000
-	#$executable 8 0 0 2 100000 50000 1500 16 1000
-	#$executable 9 0 0 2 100000 50000 1500 16 1000
-	#$executable 10 0 0 2 100000 50000 1500 16 1000
+	$executable 4 0 0 2 100000 50000 1500 16 1000
+	$executable 5 0 0 2 100000 50000 1500 16 1000
+	$executable 6 0 0 2 100000 50000 1500 16 1000
+	$executable 7 0 0 2 100000 50000 1500 16 1000
+	$executable 8 0 0 2 100000 50000 1500 16 1000
+	$executable 9 0 0 2 100000 50000 1500 16 1000
+	$executable 10 0 0 2 100000 50000 1500 16 1000
 done
 
 for name in rpmalloc-unlimit rpmalloc jemalloc lockfree-malloc tcmalloc scalloc crt nedmalloc; do
@@ -28,8 +28,8 @@ for name in rpmalloc-unlimit rpmalloc jemalloc lockfree-malloc tcmalloc scalloc 
 	$executable 6 0 1 2 100000 50000 1500 16 8000
 	$executable 7 0 1 2 100000 50000 1500 16 8000
 	$executable 8 0 1 2 100000 50000 1500 16 8000
-	#$executable 9 0 1 2 100000 50000 1500 16 8000
-	#$executable 10 0 1 2 100000 50000 1500 16 8000
+	$executable 9 0 1 2 100000 50000 1500 16 8000
+	$executable 10 0 1 2 100000 50000 1500 16 8000
 done
 
 for name in rpmalloc-unlimit rpmalloc jemalloc lockfree-malloc tcmalloc scalloc crt nedmalloc; do
@@ -43,8 +43,8 @@ for name in rpmalloc-unlimit rpmalloc jemalloc lockfree-malloc tcmalloc scalloc 
 	$executable 6 0 1 2 100000 50000 1500 16 16000
 	$executable 7 0 1 2 100000 50000 1500 16 16000
 	$executable 8 0 1 2 100000 50000 1500 16 16000
-	#$executable 9 0 1 2 100000 50000 1500 16 16000
-	#$executable 10 0 1 2 100000 50000 1500 16 16000
+	$executable 9 0 1 2 100000 50000 1500 16 16000
+	$executable 10 0 1 2 100000 50000 1500 16 16000
 done
 
 for name in rpmalloc-unlimit rpmalloc jemalloc lockfree-malloc tcmalloc scalloc crt nedmalloc; do
@@ -58,8 +58,8 @@ for name in rpmalloc-unlimit rpmalloc jemalloc lockfree-malloc tcmalloc scalloc 
 	$executable 6 0 2 2 100000 50000 1500 128 64000
 	$executable 7 0 2 2 100000 50000 1500 128 64000
 	$executable 8 0 2 2 100000 50000 1500 128 64000
-	#$executable 9 0 2 2 100000 50000 1500 128 64000
-	#$executable 10 0 2 2 100000 50000 1500 128 64000
+	$executable 9 0 2 2 100000 50000 1500 128 64000
+	$executable 10 0 2 2 100000 50000 1500 128 64000
 done
 
 for name in rpmalloc-unlimit rpmalloc jemalloc lockfree-malloc tcmalloc scalloc crt nedmalloc; do
@@ -73,6 +73,6 @@ for name in rpmalloc-unlimit rpmalloc jemalloc lockfree-malloc tcmalloc scalloc 
 	$executable 6 0 2 2 100000 50000 1500 512 160000
 	$executable 7 0 2 2 100000 50000 1500 512 160000
 	$executable 8 0 2 2 100000 50000 1500 512 160000
-	#$executable 9 0 2 2 100000 50000 5100 512 160000
-	#$executable 10 0 2 2 100000 50000 1500 512 160000
+	$executable 9 0 2 2 100000 50000 5100 512 160000
+	$executable 10 0 2 2 100000 50000 1500 512 160000
 done
