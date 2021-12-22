65f9ae0475ba02fcf29e0525d8a3930b084a24e0
Mattias Jansson <mjansson@gmail.com>
2021-12-22 15:45:07 +0100

    use stdatomic on gcc and clang

--- test/atomic.h
+++ test/atomic.h
@@ -1,6 +1,55 @@
 
 #include <stdint.h>
 
+#if defined(__clang__) || defined(__GNUC__)
+
+#include <stdatomic.h>
+
+typedef _Atomic(void*) atomicptr_t;
+typedef _Atomic(int) atomic32_t;
+
+static void*
+atomic_load_ptr(atomicptr_t* src) {
+	return atomic_load_explicit(src, memory_order_acquire);
+}
+
+static void
+atomic_store_ptr(atomicptr_t* dst, void* val) {
+	atomic_store_explicit(dst, val, memory_order_release);
+}
+
+static int32_t
+atomic_load32(atomic32_t* src) {
+	return atomic_load_explicit(src, memory_order_acquire);
+}
+
+static void
+atomic_store32(atomic32_t* dst, int32_t val) {
+	atomic_store_explicit(dst, val, memory_order_release);
+}
+
+static int32_t
+atomic_incr32(atomic32_t* val) {
+	return atomic_fetch_add_explicit(val, 1, memory_order_release) + 1;
+}
+
+static int32_t
+atomic_decr32(atomic32_t* val) {
+	return atomic_fetch_add_explicit(val, -1, memory_order_release) - 1;
+}
+
+static int32_t
+atomic_add32(atomic32_t* val, int32_t add) {
+	return atomic_fetch_add_explicit(val, add, memory_order_release) + add;
+}
+
+static int
+atomic_cas_ptr(atomicptr_t* dst, void* val, void* ref) {
+	return atomic_compare_exchange_weak_explicit(dst, &ref, val, memory_order_release, memory_order_acquire);
+}
+
+#elif defined(_MSC_VER)
+
 #if defined( __x86_64__ ) || defined( _M_AMD64 ) || defined( _M_X64 ) || defined( _AMD64_ ) || defined( __arm64__ ) || defined( __aarch64__ )
 #  define ARCH_64BIT 1
 #else
@@ -45,48 +94,36 @@ atomic_store32(atomic32_t* dst, int32_t val) {
 
 static int32_t
 atomic_incr32(atomic32_t* val) {
-#ifdef _MSC_VER
 	int32_t old = (int32_t)_InterlockedExchangeAdd((volatile long*)&val->nonatomic, 1);
 	return (old + 1);
-#else
-	return __sync_add_and_fetch(&val->nonatomic, 1);
-#endif
 }
 
 static int32_t
 atomic_decr32(atomic32_t* val) {
-#ifdef _MSC_VER
 	int32_t old = (int32_t)_InterlockedExchangeAdd((volatile long*)&val->nonatomic, -1);
 	return (old - 1);
-#else
-	return __sync_add_and_fetch(&val->nonatomic, -1);
-#endif
 }
 
 static int32_t
 atomic_add32(atomic32_t* val, int32_t add) {
-#ifdef _MSC_VER
 	int32_t old = (int32_t)_InterlockedExchangeAdd((volatile long*)&val->nonatomic, add);
 	return (old + add);
-#else
-	return __sync_add_and_fetch(&val->nonatomic, add);
-#endif
 }
 
 static int
 atomic_cas_ptr(atomicptr_t* dst, void* val, void* ref) {
-#ifdef _MSC_VER
-#  if ARCH_64BIT
+#if ARCH_64BIT
 	return (_InterlockedCompareExchange64((volatile long long*)&dst->nonatomic,
 		(long long)val, (long long)ref) == (long long)ref) ? 1 : 0;
-#  else
+#else
 	return (_InterlockedCompareExchange((volatile long*)&dst->nonatomic,
 		(long)val, (long)ref) == (long)ref) ? 1 : 0;
-#  endif
-#else
-	return __sync_bool_compare_and_swap(&dst->nonatomic, ref, val);
 #endif
 }
 
 #undef ARCH_64BIT
 #undef ALIGNED_STRUCT
+
+#else
+#  error Compiler not supported yet
+#endif
